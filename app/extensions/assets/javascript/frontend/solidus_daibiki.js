26174647e52d1e64dc5f4bd35ad3c226b3010b33
Cody Baldwin <codybaldwin@gmail.com>
2023-04-13 11:39:16 +0900

    Adding warning for use with custome javascript to disallow usage of COD by non-registered users

--- app/extensions/assets/javascript/frontend/solidus_daibiki.js
+++ app/extensions/assets/javascript/frontend/solidus_daibiki.js
@@ -1,15 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
   const daibikiDesc = document.getElementById('daibiki-description');
   if (daibikiDesc) {
+    const noUser = document.getElementById('payment')?.dataset?.hasUser === '0';
     const maxAmount = daibikiDesc.dataset.totalMax;
-    const warningMessage = daibikiDesc.dataset.warningMessage;
     const orderTotal = daibikiDesc.dataset.orderTotal;
-    if (parseInt(orderTotal) > parseInt(maxAmount)) {
+    if (parseInt(orderTotal) > parseInt(maxAmount) || noUser) {
       const daibikiOption = document.getElementById(`order_payments_attributes__payment_method_id_${daibikiDesc.dataset.paymentId}`);
       daibikiOption.disabled = true;
       daibikiOption.classList.add('opacity-50', 'cursor-not-allowed', 'bg-gray-400')
       daibikiOption.parentElement.classList.add('text-muted');
       const span = document.createElement('span');
+      const warningMessage = (parseInt(orderTotal) > parseInt(maxAmount)) ? daibikiDesc.dataset.totalExcessWarning : daibikiDesc.dataset.noUserWarning;
       span.innerText = warningMessage;
       daibikiOption.parentElement.append(span);
     }
